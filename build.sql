71b2995f260f34f479d28f9464e176c0d6093562
kevin <kevin.voyer.developpeur@gmail.com>
2021-03-04 21:05:10 +0100

    feat: create tables User and Address

--- build.sql
+++ build.sql
@@ -1,3 +1,23 @@
 DROP DATABASE IF EXISTS helpYourNeighbour;
 CREATE DATABASE helpYourNeighbour;
 
+CREATE TABLE User (
+    user_id SERIAL PRIMARY KEY,
+    firstname VARCHAR(32) NOT NULL,
+    lastname VARCHAR(64) NOT NULL,
+    password TEXT NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    backup_email VARCHAR(320) NOT NULL
+);
+
+CREATE TABLE Address (
+    address_id SERIAL PRIMARY KEY,
+    fk_user_id INTEGER,
+    number INTEGER,
+    street VARCHAR(500),
+    postal_code VARCHAR(10),
+    city VARCHAR(64),
+    date DATE,
+    FOREIGN KEY(fk_user_id) REFERENCES User(user_id)
+    ON DELETE CASCADE
+);
