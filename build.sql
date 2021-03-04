089e1155a70eedb68e3769337a70b993852dbf5a
kevin <kevin.voyer.developpeur@gmail.com>
2021-03-04 21:19:01 +0100

    feat: add if not exists on tables creations

--- build.sql
+++ build.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS helpYourNeighbour;
 CREATE DATABASE helpYourNeighbour;
 
-CREATE TABLE User (
-    user_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS PEOPLE(
+    people_id SERIAL PRIMARY KEY,
     firstname VARCHAR(32) NOT NULL,
     lastname VARCHAR(64) NOT NULL,
     password TEXT NOT NULL,
@@ -10,14 +10,14 @@ CREATE TABLE User (
     backup_email VARCHAR(320) NOT NULL
 );
 
-CREATE TABLE Address (
+CREATE TABLE IF NOT EXISTS Address (
     address_id SERIAL PRIMARY KEY,
-    fk_user_id INTEGER,
+    fk_people_id INTEGER,
     number INTEGER,
     street VARCHAR(500),
     postal_code VARCHAR(10),
     city VARCHAR(64),
     date DATE,
-    FOREIGN KEY(fk_user_id) REFERENCES User(user_id)
+    FOREIGN KEY(fk_people_id) REFERENCES PEOPLE(people_id)
     ON DELETE CASCADE
 );
